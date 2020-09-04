c40e987f54b6ca0ba019c0057fec8e2e16e3216a
xeyzo <schneidergw@yahoo.com>
2020-09-04 08:32:34 +0700

    initial commit

--- loging-02.py
+++ loging-02.py
@@ -15,30 +15,19 @@ class Log:
     def error(self):
         logger.error("We can't divide any numbers by zero.")
 
-    # def notice(self):
-    #     logger.notice("This is an information about something.")
-
     def warning(self):
         logger.warning("Insufficient funds.")
 
     def debug(self):
         logger.debug("This is debug message.")
 
-    # def alert(self):
-    #     logger.alert("This is an information about something.")
-
     def critical(self):
         logger.critical("Medic!! We've got critical damages.")
 
-    # def emergency(self):
-        # logger.emergency("This is an information about something.")
 
 log = Log()
 log.info()
 log.error()
-# log.notice()
 log.warning()
 log.debug()
-# log.alert()
 log.critical()
-# log.emergency()
